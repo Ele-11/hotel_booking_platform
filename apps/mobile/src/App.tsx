d214828b225cf1909747b939f5b17e6a41a37710
ArtiaEle <xydeng@163.com>
2026-02-04 13:55:00 +0800

    refactor ： 更新为最新的代码，合并冲突时选这个的更改

--- apps/mobile/src/App.tsx
+++ apps/mobile/src/App.tsx
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import { StatusBar } from "expo-status-bar";
-import React from 'react';
-import { Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Hello World!!!</Text>
-      <StatusBar style="auto" />
-    </View>
-=======
 import { NavigationContainer } from '@react-navigation/native';
 import { RootNavigator } from './navigation';
 
@@ -18,6 +6,5 @@ export default function App() {
     <NavigationContainer>
       <RootNavigator />
     </NavigationContainer>
->>>>>>> feature-mobile
   );
 }
